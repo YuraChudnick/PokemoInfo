28c73b8b75b1341eba9373c11df8ea5d593ec12b
Yurii Chudnovets <yura.chudnick@mail.ru>
2018-11-06 18:19:57 +0200

    Initial Commit

---
+++ PokemonInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokemonInfo
+//
+//  Created by yura on 11/6/18.
+//  Copyright © 2018 TFM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
